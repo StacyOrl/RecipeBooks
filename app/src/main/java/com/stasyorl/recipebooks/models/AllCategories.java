9a09d27b7f5b50f35603670a03a61d3000fe793a
Anastasia <orliknastushik@gmail.com>
2022-02-10 21:56:15 +0200

    comit2

---
+++ app/src/main/java/com/stasyorl/recipebooks/models/AllCategories.java
@@ -0,0 +1,30 @@
+package com.stasyorl.recipebooks.models;
+
+import java.util.ArrayList;
+
+public class AllCategories {
+
+    ArrayList<String> categoryTitle;
+    String Url;
+
+    public AllCategories(ArrayList<String> categoryTitle, String Url) {
+        this.categoryTitle = categoryTitle;
+        this.Url = Url;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+
+    public ArrayList<String> getCategoryTitle() {
+        return categoryTitle;
+    }
+
+    public void setCategoryTitle(ArrayList<String> categoryTitle) {
+        this.categoryTitle = categoryTitle;
+    }
+}
